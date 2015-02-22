894fdde14c74a3b7986356244a18af01520d5b0b
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2015-02-22 17:21:20 +0200

    Remove tabs

--- lib/monitoring_service/config.rb
+++ lib/monitoring_service/config.rb
@@ -2,14 +2,13 @@ require 'singleton'
 
 module MonitoringService
   class Config
-  	include Singleton
+    include Singleton
 
-  	attr_accessor :setting
+    attr_accessor :setting
 
-  	def load
-  	  @setting ||= YAML.load_file("#{ENV['HOME']}/.monitoring_config")
-  	  self
-  	end
+    def load
+      @setting ||= YAML.load_file("#{ENV['HOME']}/.monitoring_config")
+      self
+    end
   end
 end
-
